734d80bc50b2a1b00c52902a5ff62f6221d7f144
aschulz1674 <aschulz1674@student.phoenixunion.org>
2023-01-11 20:57:27 -0700

    baby

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Rizzlers.org</h1>
+      <br/>
+      <h4>play:</h4>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
+      <p>mc.rizzlers.org</p>
+    </div>
+  );
+}
